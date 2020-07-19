844f8463089e10c5593c5ca6a2b08ce1e66fd790
Ashish2796 <ashishgupta2796@gmail.com>
2020-07-19 20:12:15 +0530

    adding flash message (Success or error)

--- app.js
+++ app.js
@@ -54,9 +54,17 @@ passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-//set title 
+//set local variable middleware
 app.use(function(req,res,next){
   res.locals.title ='Surf-Shop';
+  //set success message
+  res.locals.success =req.session.success || '' ;
+  delete req.session.success;
+
+  //set error message
+  res.locals.error =req.session.error || '';
+  delete req.session.error;
+
   next();
 });
 
@@ -71,13 +79,16 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // // set locals, only providing error in development
+  // res.locals.message = err.message;
+  // res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  // // render the error page
+  // res.status(err.status || 500);
+  // res.render('error');
+  console.log(err);
+  req.session.error = err.message;
+  res.redirect('back');
 });
 
 module.exports = app;
