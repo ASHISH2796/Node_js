9d4278e204c63cd79defcd301ea44694cd6b07c7
Ashish2796 <ashishgupta2796@gmail.com>
2020-08-02 20:08:26 +0530

    Review inital setup

---
+++ controller/reviews.js
@@ -0,0 +1,17 @@
+const Post = require('../models/post');
+const Review =require('../models/review');
+
+module.exports ={
+    //Create review
+    async reviewCreate(req,res,next){
+
+    },
+    //Update Review
+    async reviewUpdate(req,res,next){
+
+    },
+    //Delete Review
+    async reviewDelete(req,res,next){
+
+    }
+}
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,17 +1,9 @@
 const express = require('express');
 const router = express.Router({mergeParams:true});
 
-/* GET reviews index /post/:id/reviews */
-router.get('/', (req, res, next)=>{
-    res.send("index reviews routes");
-  });
 /* POST  reviews create /post/:id/reviews */
 router.post('/', (req, res, next)=>{
     res.send("in create routes create : /post/:id/reviews ");
-  });
-/* GET  edit /post/:id/reviews/:review_id/edit */
-  router.get('/:review_id/edit', (req, res, next)=>{
-    res.send("in reviews routes edit : /:review_id/edit");
   });
   /* PUT reviewss update /reviews/:review_id */
   router.put('/:review_id', (req, res, next)=>{
